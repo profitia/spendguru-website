2a24f3bae0d7f92fe96c7c16c7d3d27879f1b426
Tomasz Uściński <tomasz.uscinski@profitia.pl>
2026-04-27 11:41:54 +0200

    fix: studio — split Server/Client components (ssr:false w Client Component)

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,15 +1,10 @@
-import dynamic from 'next/dynamic'
-import config from '../../../../sanity.config'
+import { metadata, viewport } from 'next-sanity/studio'
+import { StudioClient } from './StudioClient'
 
-export { metadata, viewport } from 'next-sanity/studio'
+export { metadata, viewport }
 
 export const dynamic = 'force-dynamic'
 
-const NextStudio = dynamic(
-  () => import('next-sanity/studio').then((mod) => mod.NextStudio),
-  { ssr: false }
-)
-
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <StudioClient />
 }
